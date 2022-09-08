5e82094d3b16d826495caad5b45b74ccf261cea2
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2022-09-08 23:57:16 +0300

    monty

--- funcOp1.c
+++ funcOp1.c
@@ -1,41 +1,45 @@
 #include "monty.h"
 
 /**
- * swap - swaps the top two elements of the stack
+ * _swap - swaps the top two elements of the stack
  * @stack: pointer to a pointer of first node
  * @line_number: file line number
  */
-void swap(stack_t **stack, unsigned int line_number)
+void _swap(stack_t **stack, unsigned int line_number)
 {
-	int num;
 	stack_t *tmp;
 
-	if (!*stack || !(*stack)->next)
-		error_handle(stack, line_number, 5);
-	num = (*stack)->n;
-	tmp = (*stack)->next;
-
-	(*stack)->n = tmp->n;
-	tmp->n = num;
+	if ((*stack)->next == NULL || !(*stack)->next->next == NULL)
+	{
+		error_set(short_stack_error(line_number, "swap"));
+		return;
+	}
+	
+	tmp = (*stack)->next->next;
+	(*stack)->next->next = tmp->next;
+	(*stack)->next->prev = tmp;
+	if (tmp->next)
+		tmp->next->prev = (*stack)->next;
+	tmp->next = (*stack)->next;
+	tmp->prev = *stack;
+	(*stack)->next = tmp;
 }
 
 /**
- * add - adds the top two elements of the stack
+ * _add - adds the top two elements of the stack
  * @stack: pointer to pointer to first node
  * @line_number: file line number
  */
-void add(stack_t **stack, unsigned int line_number)
+void _add(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp;
-
-	if (!*stack || !(*stack)->next)
-		error_handle(stack, line_number, 6);
-	tmp = *stack;
-	*stack = (*stack)->next;
-
-	(*stack)->n += tmp->n;
-	free(tmp);
-	(*stack)->prev = NULL;
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		error_set(short_stack_error(line_number, "add"));
+		return;
+	}
+	
+	(*stack)->next->next->n += (*stack)->next->n;
+	_pop(stack, line_number);
 }
 
 /**
@@ -58,22 +62,25 @@ void nop(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * divide - divides second top node by the top node of the stack
+ * _divide - divides second top node by the top node of the stack
  * @stack: pointer to first node pointer
  * @line_number: file line number
  */
-void divide(stack_t **stack, unsigned int line_number)
+void _divide(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp;
-
-	if (!*stack || !(*stack)->next)
-		error_handle(stack, line_number, 8);
-	if ((*stack)->n == 0)
-		error_handle(stack, line_number, 9);
-	tmp = *stack;
-	*stack = (*stack)->next;
-
-	(*stack)->n /= tmp->n;
-	free(tmp);
-	(*stack)->prev = NULL;
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		error_set(short_stack_error(line_number, "divide"));
+		return;
+	}
+	
+	if ((*stack)->next->->n == 0)
+	{
+		error_set(divide_errorline_number));
+		return;
+	
+	}
+	
+	(*stack)->next->next->n /= (*stack)->next->n;
+	_pop(stack, line_number);
 }
